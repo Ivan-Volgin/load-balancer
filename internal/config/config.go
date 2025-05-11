a9048e0f973b4c5b01076c81c2c106e66d4771ac
Ivan Volgin <volgin.ivan.05@gmail.com>
2025-05-11 22:03:58 +0300

    Initial commit + config loader

---
+++ internal/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+)
+
+type Config struct {
+	Port     *int     `yaml:"port"`
+	Backends []string `yaml:"backends"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading config file: %s", err)
+	}
+
+	var config Config
+	if err = yaml.Unmarshal(data, &config); err != nil {
+		return nil, fmt.Errorf("Error parsing config file: %s", err)
+	}
+
+	if len(config.Backends) == 0 {
+		return nil, fmt.Errorf("No backends found in config file. Please enter at least one.")
+	}
+
+	if config.Port == nil {
+		return nil, fmt.Errorf("No port found in config file. Please enter it.")
+	}
+
+	port := *config.Port
+	if port < 1 || port > 65535 {
+		return nil, fmt.Errorf("Invalid port number %d. Port can be between 1 and 65535", config.Port)
+	}
+
+	return &config, nil
+}
